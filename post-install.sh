9679887a2c000e093d89dd0e6d9073e4bc5fc963
chingjyu <69333371+chingjyu@users.noreply.github.com>
2026-01-29 23:25:45 +0800

    remove nginx

--- post-install.sh
+++ post-install.sh
@@ -1,13 +1,13 @@
 # 配置软件源
 sed -i 's|downloads\.immortalwrt\.org|mirror.nju.edu.cn/immortalwrt|g' /etc/apk/repositories.d/distfeeds.list
-# 添加OpenWrt-momo软件源
+# 添加 OpenWrt-momo 软件源
 wget -O - https://github.com/nikkinikki-org/OpenWrt-momo/raw/refs/heads/main/feed.sh | ash
-# 下载v2ray geodata
+# 下载 v2ray geodata
 apk add v2ray-geosite v2ray-geoip
-# 删除xray-core
+# 删除 xray-core
 /etc/init.d/xray disable
 rm /usr/bin/xray
 rm /etc/init.d/xray
-# 删除xray残留文件
+# 删除 xray 残留文件
 rm -r /etc/xray
 rm -r /usr/share/xray
