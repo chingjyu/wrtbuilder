58fb121bde2ebf3b6e392add3f5336bda19b78e2
chingjyu <69333371+chingjyu@users.noreply.github.com>
2026-01-29 23:49:09 +0800

    remove v2rayA from default setup

--- post-install.sh
+++ post-install.sh
@@ -2,12 +2,38 @@
 sed -i 's|downloads\.immortalwrt\.org|mirror.nju.edu.cn/immortalwrt|g' /etc/apk/repositories.d/distfeeds.list
 # 添加 OpenWrt-momo 软件源
 wget -O - https://github.com/nikkinikki-org/OpenWrt-momo/raw/refs/heads/main/feed.sh | ash
-# 下载 v2ray geodata
-apk add v2ray-geosite v2ray-geoip
-# 删除 xray-core
-/etc/init.d/xray disable
-rm /usr/bin/xray
-rm /etc/init.d/xray
-# 删除 xray 残留文件
-rm -r /etc/xray
-rm -r /usr/share/xray
+# 配置 NaiveProxy
+cat << 'EOF' > /etc/init.d/naiveproxy
+#!/bin/sh /etc/rc.common
+
+USE_PROCD=1
+START=99
+
+# Change these paths if your files are located elsewhere
+PROG=/usr/bin/naive
+CONF=/etc/naiveproxy/config.json
+
+start_service() {
+    procd_open_instance
+    procd_set_param command "$PROG" "$CONF"
+    procd_set_param respawn
+    procd_set_param stdout 1
+    procd_set_param stderr 1
+    procd_close_instance
+}
+
+stop_service() {
+    killall naive
+}
+EOF
+
+mkdir /etc/naiveproxy
+
+cat << 'EOF' > /etc/naiveproxy/config.json
+{
+  "listen": "socks://127.0.0.1:1080",
+  "proxy": "https://user:pass@example.com"
+}
+EOF
+
+/etc/init.d/naiveproxy enable
